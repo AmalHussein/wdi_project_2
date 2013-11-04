db53cc41cd41c6488a0dd3122d0bd76535c88374
Amal Hussein <amalhussein08@gmail.com>
2013-11-04 17:45:14 -0500

    setup twitter client, and method for finding tweets

---
+++ devise/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Devise::Application.config.secret_key_base = '03064a3676c484d56cf86622a18873ad029f976b2f9a8fc091a32a9fbfdc54c1a8cdaa42561f58341aad4cbacecd39fe9a79da865e0033cae3518d61f482fdeb'
