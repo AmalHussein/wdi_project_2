a5898f9d8db9a635fa092b2c7afcce5fed2092ea
Rahul Horé <hore.rahul@gmail.com>
2013-11-08 20:02:18 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AceEditorEmbedTest::Application.config.secret_key_base = 'eab1eaf391a9f85523e219f539986a485e157f32c7db107768e0de903679bcb4a535b4d4518f13c1b0d674457d7d5539e8f1d9d19dfeec3ffdb1011a7e0f97f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AceEditorEmbedTest::Application.config.session_store :cookie_store, key: '_ace_editor_embed_test_session'
