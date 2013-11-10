84ecfd74c02842dce574d19fcf5152b537431348
Amal Hussein <amalhussein08@gmail.com>
2013-11-10 18:30:19 -0500

    ran migration for reference table

--- db/migrate/20131110231556_create_reference_table.rb
+++ db/migrate/20131110231556_create_reference_table.rb
@@ -1,6 +1,12 @@
 class CreateReferenceTable < ActiveRecord::Migration
   def change
     create_table :reference_tables do |t|
+    	t.string :gem_name
+    	t.string :url 
+    	t.string :frequency
+    	t.string :catagory
+    	t.string :color
+    	t.timestamps
     end
   end
 end
