e3e0b89ffe7e0200dcfdf17c285659e24f79d8a3
Amal Hussein <amalhussein08@gmail.com>
2013-11-11 16:37:01 -0500

    fixing merge conflicts last change

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -38,7 +38,8 @@ class UsersController < ApplicationController
   def repos
   	#binding.pry
 		github = Github.new client_id: ENV['CLIENT_ID'] , client_secret: ENV['CLIENT_SECRET']
-		@tree = github.git_data.trees.get 'AmalHussein' , 'wdi_project_2', 'master'
+		tree = github.git_data.trees.get 'AmalHussein' , 'wdi_project_2', 'master' , recursive: true
+		@links = tree['tree'].map(&:path)
 	end 
 	
 
