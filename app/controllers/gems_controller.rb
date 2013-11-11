2e9e5ca67cf7bc67a619f3f7a16ee882db8667dc
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-11 14:13:11 -0500

    Deleted unneeded loop in index method in Gem controller

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -3,15 +3,10 @@ class GemsController < ApplicationController
 
   def index
     @gems = ['pry','HTTparty','slop','doorkeeper','rspec']
-    @gems.each do |my_gem|
-      @gem = (Gems.search my_gem).first
-    end
-    
-    ##@gem = (Gems.search 'rails').first
   end 
 
   def show
-    
+
   end
 
   def new
