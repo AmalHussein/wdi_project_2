28162f08ee0465021cf95ea2c1f163f18f509b12
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-11 14:04:34 -0500

    Looping through an array of gems now works.

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -3,7 +3,11 @@ class GemsController < ApplicationController
 
   def index
     @gems = ['pry','HTTparty','slop','doorkeeper','rspec']
-   
+    @gems.each do |my_gem|
+      @gem = (Gems.search my_gem).first
+    end
+
+    ##@gem = (Gems.search 'rails').first
   end 
 
   def show
