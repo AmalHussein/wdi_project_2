2ec7ba8b109eacd1914e5c57dd5f82caf60de649
Rahul Horé <hore.rahul@gmail.com>
2013-11-11 18:04:00 -0500

    Cleans up views

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,9 @@ class User < ActiveRecord::Base
 
 
 	def list_repos
-		github = Github.new client_id: ENV['CLIENT_ID'] , client_secret: ENV['CLIENT_SECRET']
-		@tree = github.git_data.trees.get 'AmalHussein' , 'wdi_project_2', 'master'
+		github = Github.new client_id: ENV['CLIENT_ID'], client_secret: ENV['CLIENT_SECRET']
+		tree = github.git_data.trees.get 'AmalHussein', 'wdi_project_2', 'master', recursive: true
+    @links = tree['tree'].map(&:path)
 	end 
 	
 end   
