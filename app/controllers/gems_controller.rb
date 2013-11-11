946e2926aa08c472f6c7d4ab77693b73e8b3b03a
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-11 13:50:27 -0500

    put looping logic in gem index view to iterate over each gem. Ugly but hopfully functional.

--- app/controllers/gems_controller.rb
+++ app/controllers/gems_controller.rb
@@ -3,11 +3,7 @@ class GemsController < ApplicationController
 
   def index
     @gems = ['pry','HTTparty','slop','doorkeeper','rspec']
-    @gems.each do |my_gem|
-      @gem = (Gems.search my_gem).first
-    end
-
-    @gem = (Gems.search 'rails').first
+   
   end 
 
   def show
