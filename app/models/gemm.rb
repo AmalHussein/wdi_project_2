b37f77a74b862c97a69f6f7436ee574035c9deab
Rahul Horé <hore.rahul@gmail.com>
2013-11-12 07:19:01 -0500

    Cleans up models

--- app/models/gemm.rb
+++ app/models/gemm.rb
@@ -1,3 +1,2 @@
 class Gemm < ActiveRecord::Base
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,11 @@ class User < ActiveRecord::Base
 	require 'rest-client'
 	require 'github_api'
 
-
-
 	def list_repos
 		github = Github.new client_id: ENV['CLIENT_ID'], client_secret: ENV['CLIENT_SECRET']
 		tree = github.git_data.trees.get 'AmalHussein', 'wdi_project_2', 'master', recursive: true
     @links = tree['tree'].map(&:path)
 	end 
-	
 end   
 
 
