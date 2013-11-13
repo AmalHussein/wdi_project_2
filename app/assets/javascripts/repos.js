f3a8ee3cdb5eeffbc8c367b3e8a174c4c0430cfc
Amal Hussein <amalhussein08@gmail.com>
2013-11-13 18:21:17 -0500

    still debugging ajax, but its in better shape

--- app/assets/javascripts/repos.js
+++ app/assets/javascripts/repos.js
@@ -11,28 +11,31 @@ GitSee.render_post = function(gemm_data) {
     );
 };
 
-$(document).ready(function(){
+GitSee.jquery_ajax_load = function(github_user, github_repo) { 
+  var url = "/repo/" + github_user + "/" + github_repo; 
+  $.ajax({
+    url: url, 
+    dataType: 'json', 
+    type: 'GET', 
+    success: function(data){
+      alert('hi from success story!');
+      GitSee.render_post(data);
+    }
+  });
+};
+GitSee.get_user_input = function(){
+  var github_user = $('#github_user').val(),
+      github_repo = $('#github_repo').val();
+  GitSee.jquery_ajax_load(github_user, github_repo);
+};
+
+
+$(function() {
   $('#search-button').click(function(e) {
     e.preventDefault();
-    GitSee.get_user_input = function(github_user, github_repo) {
-      var github_user = $('#github_user').val();
-      var github_repo = $('#github_repo').val();
-    };
+    GitSee.get_user_input();
   });
-  GitSee.jquery_ajax_load = function( github_user, github_repo) {
-    var url = "/repo/" + github_user + "/" + github_repo; 
-    $.ajax({
-      url: url, 
-      dataType: 'json', 
-      type: 'POST', 
-      success: function(data){
-        GitSee.render_post(data);
-      }
-    });
-  };
 });
-
-
 
 
 
