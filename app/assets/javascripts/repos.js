5e33dca64a7bf669edeae09b42459b9016230d9f
Amal Hussein <amalhussein08@gmail.com>
2013-11-13 17:06:21 -0500

    setting up ajax for search box, troubleshooting

--- app/assets/javascripts/repos.js
+++ app/assets/javascripts/repos.js
@@ -1,41 +1,38 @@
 var GitSee = {}; 
 
+GitSee.render_post = function(gemm_data) { 
+  $('#gems').append( 
+    "<div id=\"gemm-" + gemm_data.id + "\">" + 
+    "<h3>" + gemm_data.name + "</h3>" +
+    "<div>" + gemm_data.documentation_url + "</div>" +
+    "<div>" + gemm_data.project_url + "</div>" 
+    + "</div>"
+
+    );
+};
+
+$(document).ready(function(){
+  $('#search-button').click(function(e) {
+    e.preventDefault();
+    GitSee.get_user_input = function(github_user, github_repo) {
+      var github_user = $('#github_user').val();
+      var github_repo = $('#github_repo').val();
+    };
+  });
+  GitSee.jquery_ajax_load = function( github_user, github_repo) {
+    var url = "/repo/" + github_user + "/" + github_repo; 
+    $.ajax({
+      url: url, 
+      dataType: 'json', 
+      type: 'POST', 
+      success: function(data){
+        GitSee.render_post(data);
+      }
+    });
+  };
+});
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   function accordion(){
-//   var toggle = $(".accordion-toggle");
-//   var content = $(".accordion-content");
-//   content.hide();
-//   content.first().show();
-//   toggle.on("click", function(e){
-//     e.preventDefault();
-//     var targetContent = $(this).attr("href");
-//     content.slideUp();
-//     $(targetContent).slideDown();
-//   });
-// }
-
-// $(function() {
-//   accordion();
-// });
-  
 
 
