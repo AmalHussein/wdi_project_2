ee23885e8eca3028eca37c0fa6f3ad150e38e540
Amal Hussein <amalhussein08@gmail.com>
2013-11-13 18:32:37 -0500

    adding in the each loop method to the ajax call for the gems

--- app/assets/javascripts/repos.js
+++ app/assets/javascripts/repos.js
@@ -1,14 +1,23 @@
 var GitSee = {}; 
 
-GitSee.render_post = function(gemm_data) { 
-  $('#gems').append( 
-    "<div id=\"gemm-" + gemm_data.id + "\">" + 
-    "<h3>" + gemm_data.name + "</h3>" +
-    "<div>" + gemm_data.documentation_url + "</div>" +
-    "<div>" + gemm_data.project_url + "</div>" 
-    + "</div>"
+// var render_tweets = function(tweets_json_array) {
+//   var tweets;
+//    tweets = $.parseJSON(tweets_json_array);
+//   $.each(tweets, function(index, tweet) {
+//     $('#tweet-list').append('<li class="one-tweet">' + tweet + '</li>');
+//   })
+// };
 
-    );
+GitSee.render_gems = function(gemm_data) { 
+  var gems; 
+  gems = $.parseJSON(gemm_data);
+  $.each(gems, function(index, gem){
+    $('#gems').append("<div id=\"gemm-" + gem.id + "\">" + 
+    "<h3>" + gem.name + "</h3>" +
+    "<div>" + gem.documentation_url + "</div>" +
+    "<div>" + gem.project_url + "</div>" 
+    + "</div>")
+  })
 };
 
 GitSee.jquery_ajax_load = function(github_user, github_repo) { 
@@ -19,7 +28,7 @@ GitSee.jquery_ajax_load = function(github_user, github_repo) {
     type: 'GET', 
     success: function(data){
       alert('hi from success story!');
-      GitSee.render_post(data);
+      GitSee.render_gems(data);
     }
   });
 };
