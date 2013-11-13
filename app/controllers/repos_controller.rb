084715ade84bad08c1ec07b6f768f06a4d409571
Amal Hussein <amalhussein08@gmail.com>
2013-11-12 20:44:23 -0500

    added show method for the repos controller and testing in brower. it works

--- app/controllers/repos_controller.rb
+++ app/controllers/repos_controller.rb
@@ -1,16 +1,23 @@
-class Repo < ActiveRecord::Base 
+class ReposController < ApplicationController
+  respond_to :html, :json
 
-def repos	
-  	github = Github.new client_id: ENV['CLIENT_ID'] , client_secret: ENV['CLIENT_SECRET']
-  	tree = github.git_data.trees.get 'AmalHussein' , 'wdi_project_2', 'master' , recursive: true
-  	paths = tree['tree'].map(&:path)
-  	@grouped = paths.group_by { |p| File.dirname(p) }
-  	@grouped.each do |dir, paths|
-  		paths.delete_if { |path| @grouped.has_key? path }
-  		paths.map! { |path| File.basename(path) }	
-  	end
-  @grouped.delete_if { |dir, paths| paths.empty? }
- 
+  def show 
+    @repo = Repo.where('repo_username ilike ? AND repo_name ilike ?', params[:github_user], params[:github_repo]).first
+    respond_with @repo 
   end 
+
+  #not using just reference, 
+  # def repos	
+  # 	github = Github.new client_id: ENV['CLIENT_ID'] , client_secret: ENV['CLIENT_SECRET']
+  # 	tree = github.git_data.trees.get 'AmalHussein' , 'wdi_project_2', 'master' , recursive: true
+  # 	paths = tree['tree'].map(&:path)
+  # 	@grouped = paths.group_by { |p| File.dirname(p) }
+  # 	@grouped.each do |dir, paths|
+  # 		paths.delete_if { |path| @grouped.has_key? path }
+  # 		paths.map! { |path| File.basename(path) }	
+  # 	end
+  #   @grouped.delete_if { |dir, paths| paths.empty? }
+
+  # end 
 
 end 
