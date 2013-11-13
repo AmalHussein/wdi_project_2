2b7d979409fbfc2b33e02c7fec2dd22b30456aa7
Amal Hussein <amalhussein08@gmail.com>
2013-11-12 20:53:48 -0500

    Added serializer for including array of gemms in repo json object

--- app/models/repo.rb
+++ app/models/repo.rb
@@ -1,6 +1,9 @@
 class Repo < ActiveRecord::Base
 	has_and_belongs_to_many :gemms
 
+	# Overloading the as_json method
+	# to include gemms in the json object returned
+	# as_json is automatically called when you return json
 	def as_json(options={})
 		super(options.merge(include: :gemms))
 	end
