f74bb752b4cc9d5217e5b9758e014298c7055f6b
Rahul Horé <hore.rahul@gmail.com>
2013-11-14 07:41:15 -0500

    Refactors resource.js and show action for repo

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -5,12 +5,14 @@ GitSee.render_resource = function(resource_data) {
     "<div id=\"resource-" + resource_data.id + "\">" +
     "<a href=" + resource_data.site_url + ">" +
     resource_data.site_name + "</a>" +
-    "<div>" + resource_data.site_description + "</div>"
+    "<div>" + resource_data.site_description + "</div>" +
+    "</div>"
   );
 };
 
-GitSee.ajax_resource = function(github_user, github_repo, resource_name, resource_link, resource_desc) {
-  var url = "/repo/" + github_user + "/" + github_repo;
+GitSee.ajax_resource = function(resource_name, resource_link, resource_desc, github_user, github_repo) {
+  // currently hardcoded to 'repo/rubygems/gems'
+  var url = "/repo/" + github_user + "/" + github_repo; 
   $.ajax({
     url: url,
     dataType: 'json',
@@ -25,16 +27,18 @@ GitSee.ajax_resource = function(github_user, github_repo, resource_name, resourc
 };
 
 GitSee.add_new_resource = function() {
-  var resource_name, resource_link, resource_desc;
-  resource_name = $('#resource-form-name').val();
-  resource_link = $('#resource-form-url').val();
-  resource_desc = $('#resource-form-desc').val();
+  var resource_name = $('#resource-form-name').val(),
+      resource_link = $('#resource-form-url').val(),
+      resource_desc = $('#resource-form-desc').val(),
+      github_user = 'rubygems', // currently
+      github_repo = 'gems';     // hardcoded
+      debugger;
 
   $('#resource-form-name').val('');
   $('#resource-form-url').val('');
   $('#resource-form-desc').val('');
 
-  GitSee.ajax_resource(github_user, github_repo, resource_name, resource_link, resource_desc);
+  GitSee.ajax_resource(resource_name, resource_link, resource_desc, github_user, github_repo);
 };
 
 
