b414cc522579c3dae08a41f33be69849ccfc1638
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-11-14 14:52:51 -0500

    switching to development

--- app/assets/javascripts/repos.js
+++ app/assets/javascripts/repos.js
@@ -1,15 +1,27 @@
 $(document).ready(function(){
 
   $('.gem-docs').hide();
-  $('.gem-resources').hide();
+  $('.gem-resources').toggle();
 
   $('.gem-name h4').click(function(e){
    e.preventDefault();
+
    $('.gem-docs').appendTo('#canvas');
-   $('.gem-docs').show();
+   $('.gem-resources').appendTo('#canvas');
+   $('.gem-docs').toggle();
+   $('.gem-resources').toggle();
+
   })
 
 
-
+// $('#myelement').toggle(
+//  function(){
+//      $('#another-element').show("slide", { direction: "right" }, 1000);
+// }
+// ,
+// function(){
+//     $('#another-element').hide("slide", { direction: "right" }, 1000);
+// }
+// );
 
 });
