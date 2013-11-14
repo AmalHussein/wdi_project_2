da3764c96d4f79c7db60772d92e6a13e3ccae505
Amal Hussein <amalhussein08@gmail.com>
2013-11-14 17:34:29 -0500

    added in method to store users to database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,8 +22,54 @@ class UsersController < ApplicationController
 			repos_count: user['public_repos']}
 			user = User.find_or_create_by(login: user_info['login'])
 			redirect_to "/sessions/#{user.id}/create"
-          # decide whether or not to add repos at this time
-        end
+    end
+
+  # using this to save a new user to the database, or update profile for recurring user 
+	def load
+		github_user = Rails.cache.fetch("#{params['access_token']}", :expires_in => 9000.seconds) do 
+			JSON.parse(RestClient.get("https://api.github.com/user", {params: {:access_token => params[:access_token]}}))
+		end
+
+		stored_user = User.where(github_id: github_user['id']).first
+		if stored_user
+			unless stored_user.updated_at == github_user['updated_at']
+				stored_user.update_attributes(
+					name: github_user['name'],
+					login: github_user['login'],
+					url: github_user['url'],
+					html_url: github_user['html_url'],
+					repos_url: github_user['repos_url'],
+					gists_url: github_user['gists_url'],
+					avatar_url: github_user['avatar_url'],
+					public_repos: github_user['public_repos'],
+					github_id: github_user['id'],
+					followers: github_user['followers'],
+					following: github_user['following'],
+					created_at: github_user['created_at'],
+					updated_at: github_user['updated_at'],
+					email: github_user['email']
+				)
+			end
+		else
+			stored_user = User.create(
+				name: github_user['name'],
+				login: github_user['login'],
+				url: github_user['url'],
+				html_url: github_user['html_url'],
+				repos_url: github_user['repos_url'],
+				gists_url: github_user['gists_url'],
+				avatar_url: github_user['avatar_url'],
+				public_repos: github_user['public_repos'],
+				github_id: github_user['id'],
+				followers: github_user['followers'],
+				following: github_user['following'],
+				created_at: github_user['created_at'],
+				updated_at: github_user['updated_at'],
+				email: github_user['email']
+			)
+		end
+		redirect_to create_session_path(id: stored_user.id, access_token: params[:access_token])
+	end
 
   #       def show
   #       	@user = User.where(params[:id])
