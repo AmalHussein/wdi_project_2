3c56b5417e952941df0bd2968700fb14222a9d65
Rahul Horé <hore.rahul@gmail.com>
2013-11-14 14:18:53 -0500

    Updates Vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,12 @@
 class Vote < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :resource
+  before_save :voted?
+
+  private
+
+  def voted?
+    # return true if a user has already voted on a particular
+    # resource, false otherwise
+  end
 end
